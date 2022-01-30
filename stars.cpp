a123d2ab82832727d878c53405393173e9b29b86
QuantumCoded <bfields3@jscc.edu>
2022-01-30 13:37:03 -0600

    stars

---
+++ stars.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    const char *LINE1 = "   *     *     *     *";
+    const char *LINE2 = "*     *     *     *";
+
+    printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", LINE1, LINE2, LINE1, LINE2, LINE1, LINE2, LINE1, LINE2);
+
+    return 0;
+}
