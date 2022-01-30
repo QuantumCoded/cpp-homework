b9b0e54be2dce2ab87ae23e6a4e0ae6887f36615
QuantumCoded <bfields3@jscc.edu>
2022-01-30 14:01:38 -0600

    fix char pointers

--- stars.cpp
+++ stars.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    const char *LINE1 = "   *     *     *     *";
-    const char *LINE2 = "*     *     *     *";
+    const char* LINE1 = "   *     *     *     *";
+    const char* LINE2 = "*     *     *     *";
 
     printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", LINE1, LINE2, LINE1, LINE2, LINE1, LINE2, LINE1, LINE2);
 
